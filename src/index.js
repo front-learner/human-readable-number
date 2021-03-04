91894ec003cc40529bc83c6ef96a2c002899657a
front-learner <79720458+front-learner@users.noreply.github.com>
2021-03-04 19:10:46 +0300

    feat: update

--- src/index.js
+++ src/index.js
@@ -1,19 +1,16 @@
 module.exports = function toReadable (number) {
-    let numberArray = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen',]
-    let tenNumberArray = ['twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety']
+    let numberArray = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen',];
+    let tenNumberArray = ['twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
     if (number < 20) {
         return numberArray[number];
     }
 
-    if (number % 10 && number < 100) {
-        number = tenNumberArray[number/10 - 2];
-        return number;
+    if (number < 100 && number % 10) {
+       return tenNumberArray[number/10 - 2];
      }
-
+     
     if (number > 20 && number < 100) {
         let str = String(number);
-        str[0] = tenNumberArray[str[0] -2];
-        str [1] = numberArray[str[1]];
-        return `${str[0]} ${str[1]}`;
+        return `${tenNumberArray[Number(str[0]) -2]} ${numberArray[Number(str[1])]}`;
     }
 }
