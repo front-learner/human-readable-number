135568c6f07123eeb232ed16762882d17a507d10
front-learner <79720458+front-learner@users.noreply.github.com>
2021-03-04 18:57:45 +0300

    feat: update

--- src/index.js
+++ src/index.js
@@ -1,15 +1,19 @@
 module.exports = function toReadable (number) {
-      let numberArray = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen',]
+    let numberArray = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen',]
     let tenNumberArray = ['twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety']
     if (number < 20) {
         return numberArray[number];
     }
 
-    if (number >= 20 && number < 100) {
+    if (number % 10 && number < 100) {
+        number = tenNumberArray[number/10 - 2];
+        return number;
+     }
+
+    if (number > 20 && number < 100) {
         let str = String(number);
         str[0] = tenNumberArray[str[0] -2];
         str [1] = numberArray[str[1]];
-        return Number(str);
+        return `${str[0]} ${str[1]};
     }
 }
-
