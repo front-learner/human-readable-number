6e0e30f4d030950fd8eb9c66d2e1062c0108950b
front-learner <79720458+front-learner@users.noreply.github.com>
2021-03-04 22:48:59 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,7 +5,11 @@ module.exports = function toReadable (number) {
     if (number < 20) {
         return numberArray[number];
     }
-
+    
+    if (number > 100 && number < 1000 && (Number(str[1] + str[2]) === 10)) {
+        return numberArray[Math.floor(number/100)] + ' ' + 'hundred' + ' ' +  'ten'; 
+    }
+    
     if ((number % 10 === 0) && (number < 100) && (number >= 20)) {
         return tenNumberArray[(number/10) - 2];
     }
@@ -18,6 +22,10 @@ module.exports = function toReadable (number) {
         return numberArray[number/100] + ' hundred';
     }
 
+    if (number > 100 && number < 1000 && (Number(str[1] + str[2]) < 20)) {
+        return numberArray[Math.floor(number/100)] + ' ' + 'hundred' + ' ' +  numberArray[Number(str[1] + str[2])]; 
+    }
+
     if (number > 100 && (number % 10 === 0)) {
         return numberArray[Math.floor(number/100)] + ' ' + 'hundred' + ' ' + tenNumberArray[Number(str[1]) - 2];
     }
@@ -26,9 +34,8 @@ module.exports = function toReadable (number) {
         return numberArray[Math.floor(number/100)] + ' ' + 'hundred' + ' ' + tenNumberArray[Number(str[1]) - 2] + ' ' + numberArray[Number(str[2])];
     }
 
-    if (number > 100 && number < 1000 && (Number(str[1]) < 2)) {
-        return numberArray[Math.floor(number/100)] + ' ' + 'hundred' + ' ' +  numberArray[Number(str[2]) + 10]; 
+    if (number > 100 && number < 1000 && (Number(str[1] + str[2]) < 20)) {
+        return numberArray[Math.floor(number/100)] + ' ' + 'hundred' + ' ' +  numberArray[Number(str[1] + str[2])]; 
     }
-}
-
+} 
 
