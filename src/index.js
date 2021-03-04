74db31900aa8e9b443af1ebcf0e021dc3241e3c0
front-learner <79720458+front-learner@users.noreply.github.com>
2021-03-04 21:24:24 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,16 +1,21 @@
 module.exports = function toReadable (number) {
     let numberArray = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen',];
     let tenNumberArray = ['twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
-    let str = number.toString();
+    let str = String(number);
+    let result;
     if (number < 20) {
         return numberArray[number];
     }
 
-    if ((number % 10 === 0) && (number < 100) && (number >= 20) ) {
+    if ((number % 10 === 0) && (number < 100) && (number >= 20)) {
         return tenNumberArray[(number/10) - 2];
     }
 
     if (number > 20 && number < 100) {
-        return tenNumberArray[Number(str[0])] + '' + numberArray[Number(str[1])];
+      return tenNumberArray[Number(str[0])] + numberArray[Number(str[1])];  
+    }
+
+    if (number % 100 === 0) {
+        return numberArray[number/100] + 'hundred';
     }
 }
